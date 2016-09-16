8d712f40c791c54860ea7e60e4f4adb22169ffb8
Sergey <sergeybirich@gmail.com>
2016-09-16 16:59:11 +0300

    Added class Book from Lab1.12

---
+++ Lab1.13/src/web_tech_lab_1_13/Data/Book.java
@@ -0,0 +1,38 @@
+package web_tech_lab_1_13.Data;
+
+/**
+ * Created by Сергей on 16.09.2016.
+ */
+public class Book {
+    private String title;
+    private String author;
+    private int price;
+    private static int edition;
+    static {
+        edition = 1;
+    }
+    Book (String title, String author, int price) {
+        this.title = title;
+        this.author = author;
+        if (price<0) price = 0;
+        this.price = price;
+    }
+    public boolean equals(Object obj){
+        if (!(obj instanceof Book)) return false;
+
+        Book b = (Book)obj;
+        return (this.title == b.title && this.author==b.author && this.price==b.price);
+
+    }
+    public int hashCode(){
+
+        return 10 * title.hashCode() +
+                5 * author.hashCode() +
+                10*price ;
+    }
+    public String toString(){
+
+        return "Книга '" + title + " - " + author + " (цена " +  price + ")";
+    }
+}
+
