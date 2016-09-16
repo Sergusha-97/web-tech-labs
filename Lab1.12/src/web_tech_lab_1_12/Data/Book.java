843c823184e11992dea9934e594ee2200dd094a2
Sergey <sergeybirich@gmail.com>
2016-09-16 16:34:28 +0300

    Added class Book

---
+++ Lab1.12/src/web_tech_lab_1_12/Data/Book.java
@@ -0,0 +1,11 @@
+package web_tech_lab_1_12.Data;
+
+/**
+ * Created by Сергей on 16.09.2016.
+ */
+public class Book {
+    private String title;
+    private String author;
+    private int price;
+    private static int edition;
+}
