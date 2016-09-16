3693ab04c9a4a1278628e3ea679666e021a26b35
Sergey <sergeybirich@gmail.com>
2016-09-16 11:09:22 +0300

    Added class DataForChecking

---
+++ Lab1.2/src/web_tech_lab_1_2/Data/DataForChecking.java
@@ -0,0 +1,22 @@
+package web_tech_lab_1_2.Data;
+
+/**
+ * Created by Сергей on 16.09.2016.
+ */
+public class DataForChecking {
+    private double x;
+    private double y;
+
+    DataForChecking(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double GetX(){
+        return x;
+    }
+
+    public double GetY(){
+        return y;
+    }
+}
