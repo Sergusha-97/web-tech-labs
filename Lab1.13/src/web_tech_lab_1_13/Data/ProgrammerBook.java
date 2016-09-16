44c2bd6ca3a443bc8002b68b7f1c8d70f9d556b5
Sergey <sergeybirich@gmail.com>
2016-09-16 17:03:41 +0300

    Added class ProgrammerBook

---
+++ Lab1.13/src/web_tech_lab_1_13/Data/ProgrammerBook.java
@@ -0,0 +1,15 @@
+package web_tech_lab_1_13.Data;
+
+/**
+ * Created by Сергей on 16.09.2016.
+ */
+public class ProgrammerBook extends Book {
+    private String language;
+    private int level;
+    ProgrammerBook(String title, String author, int price, String language, int level){
+        super(title,author,price);
+        this.language = language;
+        if (level < 0) level = 0;
+        this.level = level;
+    }
+}
