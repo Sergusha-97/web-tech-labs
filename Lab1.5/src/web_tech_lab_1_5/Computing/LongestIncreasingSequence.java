a4b9f673a744d4b53149b5336fb47fdf2021bffb
Sergey <sergeybirich@gmail.com>
2016-10-11 22:36:50 +0300

    Added lab 1.5

---
+++ Lab1.5/src/web_tech_lab_1_5/Computing/LongestIncreasingSequence.java
@@ -0,0 +1,36 @@
+package web_tech_lab_1_5.Computing;
+
+/**
+ * Created by Сергей on 11.10.2016.
+ */
+class LongestIncreasingSequence {
+
+    static int getMinimalAmountOfRemovingElementsToObtain(int[] array) {
+        int[] lis = getInitializedLISValues(array.length);
+        int sequenceLength = findMaxValue(countOptimizedLISValues(array, lis));
+        return array.length - sequenceLength;
+    }
+
+    private static int[] getInitializedLISValues(int length) {
+        int[] lis = new int[length];
+        for (int i = 0; i < length; i++ )
+            lis[i] = 1;
+        return lis;
+    }
+
+    private static int[] countOptimizedLISValues(int[] array, int[] lis) {
+        for (int i = 1; i < array.length; i++ )
+            for (int j = 0; j < i; j++ )
+                if ( array[i] > array[j] && lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
+        return lis;
+    }
+
+    private static int findMaxValue(int[] array) {
+        int max = 0;
+        for (int element : array)
+            if (element > max)
+                max = element;
+        return max;
+    }
+}
